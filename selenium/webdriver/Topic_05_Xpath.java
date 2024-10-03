b909cdf7141d07c902992ea9d3c290ee2a978b23
thuynguyen <thuynt11494@gmail.com>
2024-10-03 07:17:09 +0700

    Topic 2 work with css and xpath

--- selenium/webdriver/Topic_05_Xpath.java
+++ selenium/webdriver/Topic_05_Xpath.java
@@ -1,9 +1,8 @@
-package webdriver.selenium.webdriver;
+package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
